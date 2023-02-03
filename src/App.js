c8990a106ea50d1a6800b181f7078fc5a485447d
fadeevvania <fadeevvania@mail.ru>
2023-02-04 01:31:23 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Main from "./Main/Main";
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
       <Footer/>
     </div>
   );
---
+++ src/Main/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+import React from 'react';
+
+const Main = () => {
+    return (
+        <footer className="Main">
+            
+        </footer >
+    )
+}
+
+export default Main;
