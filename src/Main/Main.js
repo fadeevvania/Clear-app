9fdab3c9cff8f21cfa3c8bde1bdc86fddfcc3e52
fadeevvania <fadeevvania@mail.ru>
2023-02-04 02:28:34 +0500

    MB normal верстка

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,9 +3,14 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <footer className="Main">
-            
-        </footer >
+        <main className="Main">
+        <div className="MainL">
+        <h1>Hi</h1>
+        </div>
+        <div className="MainR">
+            <h1>Hello</h1>`
+        </div>
+        </main >
     )
 }
 
