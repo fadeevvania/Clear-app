b5496e618c205022d030fc74e510b4c46d0e1b28
fadeevvania <fadeevvania@mail.ru>
2023-02-04 11:35:40 +0500

    Добавлены шрифты и обнуления

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Main from "./Main/Main";
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,12 +4,17 @@ import React from 'react';
 const Main = () => {
     return (
         <main className="Main">
-        <div className="MainL">
+            <div className="Main_Container">
+            <div className="MainL">
         <h1>Hi</h1>
         </div>
         <div className="MainR">
             <h1>Hello</h1>`
         </div>
+            </div>
+           <div className="Buttons">
+            <h1>HI</h1>
+           </div>
         </main >
     )
 }
